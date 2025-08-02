14da696ac3a477117e32e9a7c4123aea19e8b256
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2025-08-02 04:18:18 +0300

    cron.js addedass

--- src/routes/transactionsRoute.js
+++ src/routes/transactionsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { sql } from '../config/db.js';
-import {createTransaction, deleteTransaction, getSummaryByUserIdasync, getTransactionsByUserId} from '../controllers/transactionsController.js'
+import {createTransaction, deleteTransaction, getSummaryByUserId, getTransactionsByUserId} from '../controllers/transactionsController.js'
 
 const router = express.Router()
 
@@ -11,7 +11,7 @@ router.post("/",createTransaction)
 
 router.delete("/:id",deleteTransaction)
 
-router.get("/summary/:userId",getSummaryByUserIdasync)
+router.get("/summary/:userId",getSummaryByUserId)
 
 
 export default router
